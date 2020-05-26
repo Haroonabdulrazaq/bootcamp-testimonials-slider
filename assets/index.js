ff7d616c18c79b4e282414ee644e7a05f9725d28
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-05-26 17:28:52 +0100

    image slide as button click

--- assets/index.js
+++ assets/index.js
@@ -1,25 +1,22 @@
 const prev = document.getElementById("prev");
 const next = document.getElementById("next");
 const tanya = document.getElementById("tanya-wrapper");
+const john = document.getElementById("john-wrapper");
 const tanyaAuthor = document.getElementById("tanya-author");
 const tanyaImg = document.getElementById("tanya-img");
 
 prev.addEventListener("click", (event) => {
-  alert("Hello world");
+  tanya.classList.remove("tanya-wrapper");
+  tanya.classList.add("disappear");
+
+  john.classList.remove("disappear");
+  john.classList.add("john-wrapper");
 });
 
 next.addEventListener("click", (event) => {
-  console.log("Hekkos");
+  tanya.classList.add("tanya-wrapper");
+  tanya.classList.remove("disappear");
+
+  john.classList.add("disappear");
+  john.classList.remove("john-wrapper");
 });
-
-// tanyaAuthor.classList.add("disappear");
-// tanyaImg.classList.add("disappear");
-
-// tanyaTestimony.textContent =
-// " If you want to lay the best foundation best possible I'd recommend you take the course. The depth the instrcto go through is incredible,I now feel so confident about starting up as a professional Developer.";
-// tanyaAuthor.innerHTML =
-// " John Tarkpor <span >Junior Front-End Developer</span>";
-// tanyaImg.classList.add("disappear");
-
-// tanya.classList.remove("tanya-wrapper");
-// tanya.classList.add("disappear");
