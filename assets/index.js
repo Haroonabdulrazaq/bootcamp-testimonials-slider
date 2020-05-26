ad28accb8f1782b6c4986a5fbb7802f581325f4e
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-05-26 16:58:48 +0100

    Redesign the landing page

--- assets/index.js
+++ assets/index.js
@@ -1,19 +1,25 @@
 const prev = document.getElementById("prev");
 const next = document.getElementById("next");
-const tanyaTestimony = document.getElementById("tanya-testimony");
+const tanya = document.getElementById("tanya-wrapper");
 const tanyaAuthor = document.getElementById("tanya-author");
 const tanyaImg = document.getElementById("tanya-img");
 
 prev.addEventListener("click", (event) => {
-  tanyaTestimony.textContent =
-    " If you want to lay the best foundation best possible I'd recommend you take the course. The depth the instrcto go through is incredible,I now feel so confident about starting up as a professional Developer.";
-  tanyaAuthor.innerHTML =
-    " John Tarkpor <span >Junior Front-End Developer</span>";
-  tanyaImg.classList.add("disappear");
+  alert("Hello world");
 });
 
-next.addEventListener("click", (event) => {});
+next.addEventListener("click", (event) => {
+  console.log("Hekkos");
+});
 
-// tanyaTestimony.classList.add("disappear");
 // tanyaAuthor.classList.add("disappear");
 // tanyaImg.classList.add("disappear");
+
+// tanyaTestimony.textContent =
+// " If you want to lay the best foundation best possible I'd recommend you take the course. The depth the instrcto go through is incredible,I now feel so confident about starting up as a professional Developer.";
+// tanyaAuthor.innerHTML =
+// " John Tarkpor <span >Junior Front-End Developer</span>";
+// tanyaImg.classList.add("disappear");
+
+// tanya.classList.remove("tanya-wrapper");
+// tanya.classList.add("disappear");
