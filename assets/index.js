5cd78807c4f08c99a6d33de73821726d2bb97929
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-05-26 21:32:55 +0100

    Responsive to all media sizes

--- assets/index.js
+++ assets/index.js
@@ -4,17 +4,17 @@ const tanya = document.getElementById("tanya-wrapper");
 const john = document.getElementById("john-wrapper");
 
 prev.addEventListener("click", (event) => {
-  tanya.classList.remove("tanya-wrapper");
-  tanya.classList.add("disappear");
-
-  john.classList.remove("disappear");
-  john.classList.add("john-wrapper");
-});
-
-next.addEventListener("click", (event) => {
   tanya.classList.add("tanya-wrapper");
   tanya.classList.remove("disappear");
 
   john.classList.add("disappear");
   john.classList.remove("john-wrapper");
+});
+
+next.addEventListener("click", (event) => {
+  tanya.classList.remove("tanya-wrapper");
+  tanya.classList.add("disappear");
+
+  john.classList.remove("disappear");
+  john.classList.add("john-wrapper");
 });
